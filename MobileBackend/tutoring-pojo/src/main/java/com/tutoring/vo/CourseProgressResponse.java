e01f07f8e64e2f0412d6828156df1ddf8b35f0e4
Xinran1205 <id19037@bristol.ac.uk>
2025-03-11 19:31:34 +0000

    bug fixed

--- MobileBackend/tutoring-pojo/src/main/java/com/tutoring/vo/CourseProgressResponse.java
+++ MobileBackend/tutoring-pojo/src/main/java/com/tutoring/vo/CourseProgressResponse.java
@@ -6,12 +6,5 @@ import java.util.List;
 @Data
 public class CourseProgressResponse {
     private Long courseId;
-    private List<LessonProgressItem> lessonProgressList;
-
-    @Data
-    public static class LessonProgressItem {
-        private Long lessonId;
-        private String title;
-        private String status; // 如 "not_started", "in_progress", "completed"
-    }
+    private List<TutorDashboardResponse.LessonProgressItem> lessonProgressList;
 }
