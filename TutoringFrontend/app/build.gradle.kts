8c79d3c8228fdc4b614ef9fa10001df330906563
forFishbonein <1558637209@qq.com>
2025-05-02 12:52:39 +0100

    fix

--- TutoringFrontend/app/build.gradle.kts
+++ TutoringFrontend/app/build.gradle.kts
@@ -51,30 +51,29 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.navigation.compose)
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("com.google.code.gson:gson:2.10")
-    // OkHttp3 主库
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")    // OkHttp3 main library
+    implementation("com.google.code.gson:gson:2.10")    // Optional: OkHttp logging interceptor
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    // 可选：OkHttp 日志拦截器
+    // Retrofit2 main library
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    // Retrofit2 主库
+    // Retrofit2 Gson converter
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    // Retrofit2 Gson 转换器
+    // accompanist-pager
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // accompanist-pager
     implementation("com.google.accompanist:accompanist-pager:0.28.0")
-    // 渲染富文本
+    // Render rich text
     implementation("com.google.accompanist:accompanist-webview:0.28.0")
-    // 异步加载图片
+    // Asynchronous loading of images
     implementation("io.coil-kt:coil-compose:2.2.2")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    // Compose UI 测试
+    // Compose UI testing
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    // OkHttp + MockWebServer
+    // OkHttp + MockWebServer testing
     androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
